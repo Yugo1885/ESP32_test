7eb1dda39e1f147ee3978f6a4021c5c1cad35f2c
Yugo <84918859+Yugo1885@users.noreply.github.com>
2022-01-04 11:16:19 +0800

    連線逾時，無法發布

--- ifttt_line/ifttt_line.ino
+++ ifttt_line/ifttt_line.ino
@@ -1,4 +1,4 @@
-/*原作者:https://atceiling.blogspot.com/2019/07/arduino38dht-11-line.html*/
+/* 原作者:https://atceiling.blogspot.com/2019/07/arduino38dht-11-line.html */
 #include <WiFi.h>
 #include <DHT.h>
 #define DHTTYPE DHT11
@@ -14,13 +14,14 @@ String TEMP = "";
 String HUM = "";
 
 unsigned long previousMillis = 0;
-const long initerval = 2000;
+const long interval = 2000;
 const char* host = "maker.ifttt.com";
 const int httpPort = 80;
 
 void setup(){
   Serial.begin(9600);
   dht.begin();
+  WiFi.begin(ssid,password);
   //等待連線
   Serial.println("Connecting to Wifi");
   while(WiFi.status() !=WL_CONNECTED){
@@ -45,8 +46,8 @@ void loop(){
       return;
     }
   }
-  TEMP = "Temperature:"+String((int)temperature)+"oC";
-  HUM = "Humidity:"+String((int)humidity)+"%";
+  TEMP = "Temperature:"+String((int)temp)+"oC";
+  HUM = "Humidity:"+String((int)hum)+"%";
   Serial.println(TEMP);
   Serial.println(HUM);
   
@@ -57,9 +58,8 @@ void loop(){
     Serial.println("connection failed");
     return;
   }
-  //xxx-xxxx-xxx為自身金鑰
-  String getStr_line = "GET /trigger/Line_msg/with/key/XXX-XXXX-XXX?value1="+TEMP+"&value2="+HUM+" HTTP/1.1\r\n"
-  + "Host: " + host + "\r\n" + "User-Agent: BuildFailureDetectorESP32\r\n" + "Connection: close\r\n\r\n";
+  //xxx-xxxx-xxx為你的金鑰
+  String getStr_line = "POST/trigger/Line_msg/with/key/XXX-XXXX-XXX?value1="TEMP"&value2="HUM" HTTP/1.1\r\n"+ "Host: " + host + "\r\n";
   
   Serial.println(getStr_line);
   client.print(getStr_line);
